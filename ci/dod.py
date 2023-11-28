f8e68da9d42b1a9ebf43c5aa6d4b38135255c25b
JonathanFerrucho <jonathan.ferrucho@gmail.com>
2023-11-28 14:51:31 -0500

    Update dod.py

--- ci/dod.py
+++ ci/dod.py
@@ -6,7 +6,10 @@ import os
 
 async def main():
     print("failed")
-    raise Exception("failed")
+    archi1=open("datos.txt","w") 
+    archi1.write("failed") 
+    archi1.close()
+    # raise Exception("failed")
     return 1
 
 anyio.run(main)
